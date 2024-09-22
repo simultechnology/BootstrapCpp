df852ade67b5bad3684b6494ea83058f2f15c071
takatsugu.virtual_win <tissi0708@gmail.com>
2024-09-22 23:27:20 +0900

    Introduction of Learning Capability

--- MySimpleGame/main.cpp
+++ MySimpleGame/main.cpp
@@ -1,8 +1,11 @@
 #include <array>
 #include <iostream>
+#include <map>
 #include <optional>
 #include <random>
 #include <string>
+#include <tuple>
+#include <vector>
 
 enum class Choice
 {
@@ -106,8 +109,111 @@ void play()
     std::cout << "You won " << human_won << " v. computer won " << computer_won << '\n';
 }
 
+auto lookup_to_weighted_distrib(const std::vector<Choice> & choices)
+{
+    std::map<Choice, int> frequencies;
+    for (auto choice: choices)
+    {
+        ++frequencies[choice];
+    }
+    // std::discrete_distribution <- 0, 1, 2
+    std::vector<int> counts{ frequencies[Choice::Rock], frequencies[Choice::Paper], frequencies[Choice::Scissors] };
+    // 0, 1, 2
+    return std::discrete_distribution(counts.begin(), counts.end());
+}
+
+Choice wins_against(Choice choice)
+{
+    if (choice == Choice::Rock)
+    {
+        return Choice::Paper;
+    }
+    else if (choice == Choice::Paper)
+    {
+        return Choice::Scissors;
+    }
+    return Choice::Rock;
+    
+}
+
+class Learner
+{
+    // map tuple(computer human) -> vector<Choice>
+    std::map<std::tuple<Choice, Choice>, std::vector<Choice>> lookup;
+    std::optional<Choice> previous_computer_turn{};
+    std::optional<Choice> previous_human_turn{};
+
+    Choice last_computer_turn;
+
+    std::mt19937 gen{ std::random_device{}() };
+    public:
+        Choice turn()
+        {
+            // Rock v Paper -> Scissors, Scissors, Paper
+            // 0, 1/3, 2/3
+            if (previous_computer_turn && previous_human_turn)
+            {
+                auto it = lookup.find({previous_computer_turn.value(), previous_human_turn.value()});
+                if (it != lookup.end())
+                {
+                    // predict
+                    auto dist = lookup_to_weighted_distrib(it->second);
+                    int likely_human_choice = dist(gen);
+                    last_computer_turn = wins_against(static_cast<Choice>(likely_human_choice));
+                    return last_computer_turn;
+                    // std::discrete_distribution <- 0, 1, 2
+                }
+                
+            }
+            last_computer_turn = computer_turn();
+            return last_computer_turn;
+        }
+
+    void update(Choice human_turn)
+        {
+            if (previous_computer_turn && previous_human_turn)
+            {
+                lookup[{previous_computer_turn.value(), previous_human_turn.value()}].push_back(human_turn);
+            }
+            previous_computer_turn = last_computer_turn;
+            previous_human_turn = human_turn;
+        }
+};
+
+void play_learner()
+{
+    std::cout << "Rock(0), Paper(1), Scissors(2)\n";
+    int turns{};
+    int human_won{};
+    int computer_won{};
+    Learner learner;
+    while (true)
+    {
+        auto human = human_turn();
+        if (!human)
+        {
+            break;
+        }
+        ++turns;
+        auto computer = learner.turn();
+        learner.update(human.value());
+        std::cout << *human << " v. " << computer << '\n';
+        if (first_player_wins(human.value(), computer))
+        {
+            std::cout << "You chose: " << *human << ". You won!\n";
+            ++human_won;
+        }
+        if (first_player_wins(computer, human.value()))
+        {
+            std::cout << "You chose: " << *human << ". You lost!\n";
+            ++computer_won;
+        }
+    }
+    std::cout << "You won " << human_won << " v. computer won " << computer_won << '\n';
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "start!";
-    play();
+    play_learner();
 }
